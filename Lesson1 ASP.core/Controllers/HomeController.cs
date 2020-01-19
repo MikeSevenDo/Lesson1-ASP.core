315e79ef5f1ed8558aec992856a09a4350c26712
Миша Семиделихин <mishasam97@yandex.ru>
2020-01-19 16:56:56 +1000

    Creating View, Model, Controller

---
+++ Lesson1 ASP.core/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly List<EmployeeModel> employees = new List<EmployeeModel>()
+        {
+            new EmployeeModel
+            {
+                Id = 1, 
+                Age = 22, 
+                FirstName = "Миша", 
+                SurName = "Семиделихин1", 
+                Patronymic = "Сергеевич", 
+                Position = "Человек"
+            },
+            new EmployeeModel{
+                Id = 2, 
+                Age = 25, 
+                FirstName = "Миша2", 
+                SurName = "Семиделихин2", 
+                Patronymic = "Сергеевич", 
+                Position = "Человек"
+            }
+        };
+
+
+
+
+        /// <summary>
+        /// Get "home/index"
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View(employees);
+        }
+    }
+}
--- Lesson1 ASP.core/Startup.cs
+++ Lesson1 ASP.core/Startup.cs
@@ -5,17 +5,26 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Lesson1_ASP.core
+namespace WebStore
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration conf)
+        {
+            configuration = conf;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +39,7 @@ namespace Lesson1_ASP.core
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
